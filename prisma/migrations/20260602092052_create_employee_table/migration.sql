67c656ff7ff5cb16730360c382473ed9519a2969
harsh.d.p <harsh.d.p@samsung.com>
2026-06-02 14:52:08 +0530

    add employee prisma model

---
+++ prisma/migrations/20260602092052_create_employee_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "employeeCode" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "department" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "salary" DECIMAL(12,2) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_employeeCode_key" ON "Employee"("employeeCode");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
+
+-- CreateIndex
+CREATE INDEX "Employee_country_department_idx" ON "Employee"("country", "department");
