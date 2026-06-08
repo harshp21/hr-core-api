cb61f2ee5613254bd3456d17bc324302012101e5
harsh.d.p <harsh.d.p@samsung.com>
2026-06-08 10:42:42 +0530

    feat(analytics): add service and query validation logic

---
+++ src/modules/analytics/analytics.repository.interface.ts
@@ -0,0 +1,10 @@
+import { DepartmentSalaryInsight, JobTitleSalaryInsight } from './analytics.schema';
+import { CountrySalaryInsight } from './analytics.types';
+
+export interface AnalyticsRepository {
+  getCountrySalaryInsights(): Promise<CountrySalaryInsight[]>;
+
+  getJobTitleSalaryInsights(country: string): Promise<JobTitleSalaryInsight[]>;
+
+  getDepartmentInsights(): Promise<DepartmentSalaryInsight[]>;
+}
---
+++ src/modules/analytics/analytics.service.ts
@@ -0,0 +1,17 @@
+import { AnalyticsRepository } from './analytics.repository.interface';
+
+export class AnalyticsService {
+  constructor(private readonly analyticsRepository: AnalyticsRepository) {}
+
+  async getCountrySalaryInsights() {
+    return this.analyticsRepository.getCountrySalaryInsights();
+  }
+
+  async getJobTitleSalaryInsights(country: string) {
+    return this.analyticsRepository.getJobTitleSalaryInsights(country);
+  }
+
+  async getDepartmentInsights() {
+    return this.analyticsRepository.getDepartmentInsights();
+  }
+}
